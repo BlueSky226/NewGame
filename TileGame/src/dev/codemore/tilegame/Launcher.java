5563ad03e5aa225cae4406f5b5a754a71da9e763
unw <unw@DESKTOP-UTP7EE8>
2020-08-08 13:29:18 +0530

    Window created

---
+++ TileGame/src/dev/codemore/tilegame/Launcher.java
@@ -0,0 +1,12 @@
+package dev.codemore.tilegame;
+
+import dev.codemore.tilegame.display.Display;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		//new Display("Title",300,300);
+		new Game("Title Game!",400,400);
+	}
+
+}
